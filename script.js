456a845cf37417a9601eab866a61b094ce3909c7
Ethan Vazquez <Ethan.Vazquez050@gmail.com>
2023-05-30 11:54:14 -0700

    Added data for Smash Games

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("Hello World")
+// Index 
+//
+
+const data = {
+    brandName: "Smash Games",
+    pages: [
+        {
+            name: "index",
+            pageName: "Home",
+            callToAction: {
+                src: "images/SmashGamesLogo-short.png",
+                altText: "Smash Games Logo"
+            }
+        },
+        {
+            name: "blast",
+            pageName: "Inferno Blast",
+            callToAction: {
+                src: "images/Inferno-Jumbotron.png",
+                altText: "Inferno Blast Gameplay",
+                buttonLinkSrc: "https://steampowered.com",
+                buttonLinkText: "Buy now on Steam",
+            }
+        }
+    ]
+    
+}
